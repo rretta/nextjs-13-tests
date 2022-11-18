ccd22fe5c296253bfbaa2c17334674b50e03e99b
camelCase <caracido@gmail.com>
2022-11-18 20:02:43 -0300

    first commit

---
+++ components/Navigation.jsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import styles from "./Navigation.module.css"
+
+
+const links = [
+  {
+    label: "Home",
+    route: "/",
+  },
+  {
+    label: "About",
+    route: "/about",
+  },
+  {
+    label: "Posts",
+    route: "/posts",
+  },
+];
+
+export default function Navigation() {
+  return (
+    <header className={styles.header}>
+      <nav className={styles.navigation}>
+        {links.map(({ label, route }) => (
+          <li key={route}>
+            <Link href={route}>{label}</Link>
+          </li>
+        ))}
+      </nav>
+    </header>
+  );
+}
